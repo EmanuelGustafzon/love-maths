9b396b2ae43f2043193a56ff083e22912dfdbbfa
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2022-06-01 15:42:12 +0000

    add multiply code

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,10 +27,17 @@ let num2 = Math.floor(Math.random() * 25) + 1;
 
 if (gameType === "addition") {
     displayAdditionQuestion(num1, num2);
-} else {
+} else if (gameType === "multiply") {
+    displayMultiplyQuestion(num1, num2);
+} else if (gameType === "substract") {
+    displaySubstractQuestion(num1, num2);
+}
+ else {
     alert(`Unknown game type: ${gameType}`)
     throw `Unknown game type:  ${gameType}. Aborting!`;
 }
+
+
 
 }
 /**
@@ -66,6 +73,10 @@ if (gameType === "addition") {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
+    } else if (operator === "*") {
+        return [operand1 * operand2, "multiply"];
+    } else if (operator === "-") {
+        return [operand1 - operand2, "substract"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -99,10 +110,14 @@ function displayAdditionQuestion (operand1, operand2) {
 
 
 
-function displaySubstractQuestion () {
-    
+function displaySubstractQuestion (operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '-';
 }
 
-function displayMultiplyQuestion () {
-    
+function displayMultiplyQuestion (operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '*';
 }
